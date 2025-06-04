cb8b3a7474563bf377e7945c7dc8736eb07aa7c5
SamahaMunir <119500740+SamahaMunir@users.noreply.github.com>
2025-06-04 22:44:13 +0500

    Create activity.js and dbConn.js

---
+++ ecomate/backend/models/Activity.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose'
+import seq from 'mongoose-sequence';
+
+
+
+const AutoIncrement = seq(mongoose);
+
+const activitySchema = new mongoose.Schema({
+  user: {
+     type: mongoose.Schema.Types.ObjectId, 
+     ref: 'User',
+      required: true 
+},
+  type: {
+     type: String,
+    required: true 
+}, // "transport", "energy", "diet", "positive"
+  description: String,
+  carbonImpactKg: Number, // positive or negative
+  date: { 
+     type: Date,
+     default: Date.now 
+}
+},{ timestamps: true });
+
+activitySchema.plugin(AutoIncrement, {
+  inc_field: 'activityId'
+});
+
+export default mongoose.model('Activity', activitySchema);
